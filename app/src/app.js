568248e442e4777725d1af7777491724dbcad9dd
Christina Daskalaki <chdask@gmail.com>
2016-07-10 19:59:35 +0100

    init commit

---
+++ app/src/app.js
@@ -0,0 +1,462 @@
+'use strict';
+
+var kafkaZooUIApp = angular.module('kafkaZooUIApp', [
+  'ui.ace',
+  'angularSpinner',
+  'angularUtils.directives.dirPagination',
+  'ngRoute',
+  'ngMaterial',
+  'ngAnimate',
+  'ngAria',
+  'base64'
+])
+
+kafkaZooUIApp.controller('MenuCtrl', function ($scope) {
+   $scope.apps = [];
+   var thisApp = "Kafka Topics"
+   angular.forEach(ENV.APPS, function (app) {
+      if (app.enabled && !(app.name == thisApp)) {
+         $scope.apps.push(app);
+      }
+   });
+
+   $scope.disableAppsMenu = true;
+   if ($scope.apps.length > 0) {
+      $scope.disableAppsMenu = false;
+   }
+  });
+
+
+  kafkaZooUIApp.factory('kafkaZooFactory', function ($rootScope, $mdToast, $http, $log, $q) {
+
+  var last = {
+    bottom: false,
+    top: true,
+    left: false,
+    right: true
+  };
+
+  $rootScope.showCreateSubjectButton = true;
+  $rootScope.toastPosition = angular.extend({}, last);
+
+  // Factory should return
+  return {
+
+    sanitizePosition: function () {
+      var current = $rootScope.toastPosition;
+
+      if (current.bottom && last.top) current.top = false;
+      if (current.top && last.bottom) current.bottom = false;
+      if (current.right && last.left) current.left = false;
+      if (current.left && last.right) current.right = false;
+
+      last = angular.extend({}, current);
+    },
+    hideToast: function () {
+      $mdToast.hide();
+    },
+    showSimpleToast: function (message) {
+      $mdToast.show(
+        $mdToast.simple()
+          .textContent(message)
+          .position(this.getToastPosition())
+          .hideDelay(4000)
+      );
+    }
+    ,
+    getToastPosition: function () {
+      this.sanitizePosition();
+
+      return Object.keys($rootScope.toastPosition)
+        .filter(function (pos) {
+          return $rootScope.toastPosition[pos];
+        })
+        .join(' ');
+    },
+
+    getTopicList: function (onlyControlTopics) {
+
+      var deferred = $q.defer();
+      var getData = {
+        method: 'GET',
+        url: ENV.KAFKA_REST + '/topics'
+      };
+      $log.info('curl ' + ENV.KAFKA_REST + '/topics');
+
+      setTimeout(function () {
+        var topicList = []; // An array holding all cached subjects
+
+        //deferred.notify("Getting topics");
+        $http(getData)
+          .then(
+            function successCallback(response) {
+              $log.debug("Got " + response.data.length + " topic name's from kafka-rest : " + JSON.stringify(response.data));
+              var urlCalls = [];
+              angular.forEach(response.data, function (topicName) {
+
+                // 35 Control Topics
+                var isControlTopic = topicName.startsWith("_confluent-controlcenter", 0);
+                if (!isControlTopic && !onlyControlTopics) {
+                  $log.debug("Normal topic : " + topicName);
+                  urlCalls.push($http.get(ENV.KAFKA_REST + '/topics/' + topicName));
+                }
+                if (onlyControlTopics && isControlTopic) {
+                  $log.debug("Control topic : " + topicName);
+                  urlCalls.push($http.get(ENV.KAFKA_REST + '/topics/' + topicName));
+                }
+              });
+              $q.all(urlCalls).then(function (results) {
+                angular.forEach(results, function (result) {
+                  // $log.debug("Got" + JSON.stringify(result.data));
+                  if (result.data.name.startsWith("_confluent-controlcenter-0-", 0)) {
+                    result.data.shortName = result.data.name
+                      .replace('_confluent-controlcenter-0-', '...')
+                      // .replace('aggregate-topic-partition', 'aggregate-topic')
+                      .replace('MonitoringMessageAggregatorWindows', 'monitor-msg')
+                      .replace('aggregatedTopicPartitionTableWindows', 'aggregate-window')
+                      .replace('monitoring-aggregate-rekey', 'monitor-rekey')
+                      .replace('MonitoringStream', 'monitor-stream')
+                      .replace('MonitoringVerifierStore', 'monitor-verifier')
+                      .replace('...Group', '...group')
+                      .replace('FIFTEEN_SECONDS', '15sec')
+                      .replace('ONE_HOUR', '1hour')
+                      .replace('ONE_WEEK', '1week');
+                  } else {
+                    result.data.shortName = result.data.name;
+                  }
+                  topicList.push(result.data);
+                  // {"
+                  //   name":"connect-test","
+                  //   configs":{},
+                  //   "partitions":[
+                  //     {
+                  //          "partition":0,
+                  //          "leader":0,
+                  //           "replicas":[
+                  //               {
+                  //                  "broker":0,
+                  //                  "leader":true,
+                  //                  "in_sync":true
+                  //               }
+                  //           ]
+                  //      },{"partition":1,"...
+
+                });
+                $rootScope.showSpinner = false;
+                $log.debug("Resolved " + topicList.length + " topic details");
+                deferred.resolve(topicList);
+              });
+              // $scope.aceString = angular.toJson(response.data, true);
+            },
+            function errorCallback(response) {
+              $log.error("Error in getting topics from kafka-rest : " + JSON.stringify(response));
+              kafkaZooFactory.showSimpleToast("This is not JSon");
+            });
+
+      }, 10);
+
+      return deferred.promise;
+
+    }
+
+    //   var data = '{"name": "my_consumer_instance", "format": "avro", "auto.offset.reset": "smallest"}';
+    //   var curlCreateConsumer = 'curl -X POST -H "Content-Type: application/vnd.kafka.v1+json" \ ' +
+    //     "--data '" + data + "' \ " +
+    //     ENV.KAFKA_REST + '/consumers/rest_proxy_ui_consumer';
+    //   $log.debug(curlCreateConsumer);
+    //
+    //   // EXECUTE-1
+    //   $http(postCreateAvroConsumer)
+    //     .then(
+    //       function successCallback(response) {
+    //         // this callback will be called asynchronously when the response is available
+    //         $log.info("Success in creating avro consumer " + JSON.stringify(response));
+    //       },
+    //       function errorCallback(response) {
+    //         $log.info("Error in deleting avro consumer : " + JSON.stringify(response) +
+    //           "\n data: " + JSON.stringify(response.data) +
+    //           "\n status: " + response.status +
+    //           "\n headers: " + response.headers +
+    //           "\n config: " + JSON.stringify(response.headers) +
+    //           "\n statusText: " + response.statusText);
+    //       }
+    //     );
+    //
+    //   // return $http(postCreateAvroConsumer)
+    //   //   .then(function (response) {
+    //   //     return response.data;
+    //   //   }, function (response) {
+    //   //     return response;
+    //   //   });
+    // }
+
+  }
+});
+
+// $httpProvider lives only in .config
+kafkaZooUIApp.config(function ($routeProvider, $httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/about.html'
+      // controller: 'MainCtrl'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html'
+      // controller: 'MainCtrl'
+    })
+    .when('/create-topic', {
+      templateUrl: 'partials/create-topic.html',
+      controller: 'HeaderCtrl'
+    })
+    .when('/topic/:topicName', {
+      templateUrl: 'partials/topic.html',
+      controller: 'ViewTopicCtrl'
+    }).otherwise({
+    redirectTo: '/'
+  });
+  // $locationProvider.html5Mode(true);
+
+});
+
+kafkaZooUIApp.controller('AboutCtrl', function ($scope, $routeParams, $mdToast, $log) {
+  $log.debug("AboutCtrl - initializing");
+  $mdToast.hide();
+  //kafkaZooFactory.visibleCreateSubjectButton(true);
+});
+
+kafkaZooUIApp.controller('MainCtrl', function ($scope, $routeParams, $mdToast, $log, kafkaZooFactory) {
+    $log.debug("MainCtrl - initializing");
+    $mdToast.hide();
+
+    var promise = kafkaZooFactory.getTopicList(false);
+    promise.then(function (allTopics) {
+      $log.debug('Success fetching allTopics ' + JSON.stringify(allTopics));
+      //$scope.subjectObject = selectedSubject;
+      //$scope.aceString = angular.toJson(selectedSubject.Schema, true);
+      //$scope.multipleVersionsOn = $scope.subjectObject.otherVersions.length > 0;
+      $scope.topics = allTopics;
+    }, function (reason) {
+      $log.error('Failed: ' + reason);
+    }, function (update) {
+      $log.info('Got notification: ' + update);
+    });
+
+    $scope.countPartitionsForTopic = function (topicObj) {
+      //$log.debug('Counting partitions for topic : ' + JSON.stringify(topicObj));
+      if ((topicObj == undefined) || (topicObj.partitions == undefined)) {
+        return 0;
+      } else {
+        return Object.keys(topicObj.partitions).length;
+      }
+    };
+
+    // $scope.topics = ENV.topics;
+    //schemaRegistryFactory.visibleCreateSubjectButton(true);
+  }
+);
+
+kafkaZooUIApp.controller('HeaderCtrl', function ($scope, $rootScope, $http, $log) {
+  $log.debug("HeaderCtrl initiating");
+  $scope.schemaRegistryURL = ENV.SCHEMA_REGISTRY;
+  $scope.config = {};
+  $scope.connectionFailure = false;
+  $scope.noSubjectName = true;
+  $rootScope.showCreateSubjectButton = false;
+
+});
+
+kafkaZooUIApp.controller('ViewTopicCtrl', function ($scope, $routeParams, $log, $mdToast, $http, $base64, kafkaZooFactory) {
+
+    $scope.topicName = $routeParams.topicName;
+    $log.info("ViewTopicCtrl - initializing for topic : " + $scope.topicName);
+    $mdToast.hide();
+    $scope.kafkaDefaults = KAFKA_DEFAULTS; //TODO
+    $scope.showSpinner = false;
+    $scope.topicsOn = true;
+    $scope.zookeeperInfo = "zookeeper.landoop.com.info.goes.here";
+    $scope.brokers = ENV.BROKERS;
+
+    $scope.isAvro = false;
+    $scope.isJson = false;
+    $scope.isBinary = false;
+
+    $scope.changeView = function () {
+      $scope.topicsOn = !$scope.topicsOn;
+    };
+
+    function bytesToSize2(bytes) {
+      var sizes = ['n/a', 'bytes', 'KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+      var i = +Math.floor(Math.log(bytes) / Math.log(1024));
+      return (bytes / Math.pow(1024, i)).toFixed(i ? 1 : 0) + ' ' + sizes[isNaN(bytes) ? 0 : i + 1];
+    }
+
+    // KAFKA-REST CONSUME
+    $scope.startFetchingData = function (messagetype, topicName, consumer) {
+
+      if (['avro', 'json', 'binary'].indexOf(messagetype) < 0) {
+        $log.error("Unsupported message-type [" + messagetype + "]");
+      }
+
+      var instance = "instance";
+      var acceptMessageType = 'application/vnd.kafka.' + messagetype + '.v1+json';
+
+      var getData = {
+        method: 'GET',
+        url: ENV.KAFKA_REST + '/consumers/' + consumer + '/instances/' + instance + '/topics/' + topicName,
+        headers: {'Accept': acceptMessageType}
+      };
+      $log.debug(getData);
+      var curlGetAvroData = 'curl -vs --stderr - -X GET -H "Accept: ' + acceptMessageType + '" ' +
+        ENV.KAFKA_REST + '/consumers/' + consumer + '/instances/' + instance + '/topics/' + topicName;
+      $log.info(curlGetAvroData);
+
+      // [{"key":null,"value":{"name":"testUser"},"partition":0,"offset":0}]
+      // EXECUTE-2
+      $http(getData)
+        .then(
+          function successCallback(response) {
+            $log.info("Success in consuming " + messagetype + " data " + bytesToSize2(JSON.stringify(response).length));
+            if (messagetype == "binary") {
+              var data = response.data;
+              var data2 = angular.forEach(data, function (d) {
+                d.key = $base64.decode(d.key);
+                d.value = $base64.decode(d.value);
+              });
+              $scope.aceString = angular.toJson(data2, true);
+            } else {
+              $scope.aceString = angular.toJson(response.data, true);
+            }
+          },
+          function errorCallback(response) {
+            $log.error("Error in consuming " + messagetype + " data : " + JSON.stringify(response));
+            if (response.data.error_code == 50002) {
+              kafkaZooFactory.showSimpleToast("This is not JSon");
+            } else {
+              kafkaZooFactory.showSimpleToast("This is not Avro")
+            }
+          });
+
+    };
+
+    // 1. Create a consumer for Avro data, starting at the beginning of the topic's log.
+    // 2. Then consume some data from a topic, which is decoded, translated to JSON, and included in the response.
+    // The schema used for deserialization is fetched automatically from the schema registry.
+    // 3. Finally, clean up.
+    // [ avro | json | binary ]
+    $scope.consumeKafkaRest = function (messagetype, topicName) {
+
+      var instance = "instance"; // For creating new --from-beginning
+      var d = new Date();
+      var consumer = "Consumer-" + d.getTime();
+
+      var messageContentType;
+      if (messagetype == "avro") {
+        messageContentType = 'application/vnd.kafka.v1+json';
+      } else if (messagetype == "json") {
+        messageContentType = 'application/vnd.kafka.v1+json';
+      } else if (messagetype == "binary") {
+        messageContentType = 'application/vnd.kafka.binary.v1+json';
+      } else {
+        $log.error("Unsupported type at consumeKafkaRest(messagetype)");
+      }
+
+      var data = '{"name": "' + instance + '", "format": "' + messagetype + '", "auto.offset.reset": "smallest"}';
+      var postCreateConsumer = {
+        method: 'POST',
+        url: ENV.KAFKA_REST + '/consumers/' + consumer + '-' + messagetype,
+        data: data,
+        headers: {'Content-Type': messageContentType}
+      };
+      $log.debug(postCreateConsumer);
+
+      var curlCreateConsumer = 'curl -X POST -H "Content-Type: ' + messageContentType + '" ' +
+        "--data '" + data + "' " + ENV.KAFKA_REST + '/consumers/' + consumer + '-' + messagetype;
+      $log.info(curlCreateConsumer);
+
+      // EXECUTE-1
+      $http(postCreateConsumer)
+        .then(
+          function successCallback(response) {
+            // this callback will be called asynchronously when the response is available
+            $log.info("Success in creating avro consumer " + JSON.stringify(response));
+            $scope.startFetchingData(messagetype, topicName, consumer + "-" + messagetype);
+          },
+          function errorCallback(response, statusText) {
+            if (response.status == 409) {
+              $log.info("409 detected! " + response.data.message);
+              kafkaZooFactory.showSimpleToast(response.data.message);
+              $scope.startFetchingData(messagetype, topicName, consumer + "-" + messagetype);
+            }
+          }
+        );
+
+
+      // EXECUTE-3
+      // $http(deleteMyAvroConsumer)
+      //   .then(
+      //     function successCallback(response) {
+      //       $log.info("Success in deleting avro consumer " + JSON.stringify(response));
+      //     },
+      //     function errorCallback(response) {
+      //       // called asynchronously if an error occurs
+      //       // or server returns response with an error status.
+      //       $log.error("Error in creating avro consumer : " + JSON.stringify(error));
+      //     }
+      //   )
+
+
+      // var deleteMyAvroConsumer = {
+      //   method: 'DELETE',
+      //   url: ENV.KAFKA_REST + '/consumers/rest_proxy_ui_consumer/instances/my_consumer_instance'
+      // };
+      //
+      // $log.debug(deleteMyAvroConsumer);
+
+      //$log.info("Success in creating avro consumer " + JSON.stringify(response));
+    };
+
+    // TOPICS
+    $scope.selectedTopic;
+    $scope.selectTopic = function (topicObj) {
+      $scope.selectedTopic = topicObj
+    };
+
+    $scope.getLeader = function (partitions) {
+      if (partitions.length > 0) return partitions[0];
+    };
+
+    $scope.getTailPartitions = function (partitions) {
+      return partitions.slice(1);
+    };
+
+    $scope.getKafkaDefaultValue = function (key) {
+      var defaultValue;
+      angular.forEach(KAFKA_DEFAULTS, function (item) {
+        if (item.property == key) {
+          defaultValue = item.default;
+        }
+      });
+      return defaultValue;
+    };
+
+    $scope.getKafkaDefaultDescription = function (key) {
+      var defaultValue;
+      angular.forEach(KAFKA_DEFAULTS, function (item) {
+        if (item.property == key) {
+          defaultValue = item.description;
+        }
+      });
+      return defaultValue;
+    };
+
+    // BROKERS
+    $scope.selectedBroker;
+    $scope.selectBroker = function (brokerObj) {
+      $scope.selectedBroker = brokerObj
+    }
+
+  }
+); //end of controller
---
+++ app/src/env.js
@@ -0,0 +1,275 @@
+var ENV = {
+  BASE_URL: "http://localhost:9000",
+  KAFKA_REST: "https://kafka-rest-proxy.demo.landoop.com",
+
+
+    APPS : [
+      {
+          name : "Schema Registry",
+          enabled : true,
+          url : "###",
+          icon : "fa-file-text"
+      },
+      {
+          name : "Kafka Connectors",
+          enabled : false,
+          url : "###",
+          icon: "fa-random"
+      },
+      {
+          name : "Kafka Topics",
+          enabled : true,
+          url : "###",
+          icon : "fa-th-list"
+      }
+    ],
+  // kafka-rest-proxy.demo.landoop.com
+
+// # Get a list of topics
+// $ curl http://kafka-rest-proxy.demo.landoop.com/topics
+//   ["test","test2","test3"]
+//
+// # Get info about one topic
+// $ curl http://kafka-rest-proxy.demo.landoop.com/topics/__consumer_offsets
+// {"name":"connect-test","configs":{},"partitions":[{"partition":0,"leader":0,"replicas":[{"broker":0,"leader":true,"in_sync":true}]},{"partition":1,"leader":0,"replicas":[{"broker":1,"leader":true,"in_sync":true}]}]}
+//
+// # Get info about a topic's partitions
+// $ curl "http://localhost:8082/topics/test/partitions
+//   [{"partition":0,"leader":0,"replicas":[{"broker":0,"leader":true,"in_sync":true}]},{"partition":1,"leader":0,"replicas":[{"broker":1,"leader":true,"in_sync":true}]}]
+
+  topics: [{
+    "name": "connect-configs",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "_schemas",
+    "config": {
+      "cleanup.policy": "compact"
+    },
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "coyote_test_json",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "coyote_test_binary",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "connect-offsets",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "demo-sql-inject",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "demo-evolution",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "demo-person-1pc",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "demo-person",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "demo-reserved",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "coyote_test_avro",
+    "config": {},
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }, {
+    "name": "__consumer_offsets",
+    "config": {
+      "segment.bytes": "104857600",
+      "compression.type": "uncompressed",
+      "cleanup.policy": "compact"
+    },
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "45": [0],
+      "34": [0],
+      "12": [0],
+      "8": [0],
+      "19": [0],
+      "23": [0],
+      "4": [0],
+      "40": [0],
+      "15": [0],
+      "11": [0],
+      "9": [0],
+      "44": [0],
+      "33": [0],
+      "22": [0],
+      "26": [0],
+      "37": [0],
+      "13": [0],
+      "46": [0],
+      "24": [0],
+      "35": [0],
+      "16": [0],
+      "5": [0],
+      "10": [0],
+      "48": [0],
+      "21": [0],
+      "43": [0],
+      "32": [0],
+      "49": [0],
+      "6": [0],
+      "36": [0],
+      "1": [0],
+      "39": [0],
+      "17": [0],
+      "25": [0],
+      "14": [0],
+      "47": [0],
+      "31": [0],
+      "42": [0],
+      "0": [0],
+      "20": [0],
+      "27": [0],
+      "2": [0],
+      "38": [0],
+      "18": [0],
+      "30": [0],
+      "7": [0],
+      "29": [0],
+      "41": [0],
+      "3": [0],
+      "28": [0]
+    }
+  }, {
+    "name": "__confluent.support.metrics",
+    "config": {
+      "retention.ms": "31536000000"
+    },
+    "partitionsVersion": "1",
+    "configVersion": "1",
+    "partitions": {
+      "0": [0]
+    }
+  }]
+};
+
+var KAFKA_DEFAULTS =
+  [{
+    "property": "cleanup.policy",
+    "default": "delete",
+    "serverDefaultProperties": "log.cleanup.policy",
+    "description": "A string that is either \"delete\" or \"compact\". This string designates the retention policy to use on old log segments. The default policy (\"delete\") will discard old segments when their retention time or size limit has been reached. The \"compact\" setting will enable log compaction on the topic."
+  }, {
+    "property": "delete.retention.ms",
+    "default": "86400000",
+    "serverDefaultProperties": "log.cleaner.delete.retention.ms",
+    "description": "The amount of time to retain delete tombstone markers for log compacted topics. This setting also gives a bound on the time in which a consumer must complete a read if they begin from offset 0 to ensure that they get a valid snapshot of the final stage (otherwise delete tombstones may be collected before they complete their scan). Default is 24 hours"
+  }, {
+    "property": "flush.messages",
+    "default": "None",
+    "serverDefaultProperties": "log.flush.interval.messages",
+    "description": "This setting allows specifying an interval at which we will force an fsync of data written to the log. For example if this was set to 1 we would fsync after every message; if it were 5 we would fsync after every five messages. In general we recommend you not set this and use replication for durability and allow the operating system's background flush capabilities as it is more efficient. This setting can be overridden on a per-topic basis (see the per-topic configuration section)."
+  }, {
+    "property": "flush.ms",
+    "default": "None",
+    "serverDefaultProperties": "log.flush.interval.ms",
+    "description": "This setting allows specifying a time interval at which we will force an fsync of data written to the log. For example if this was set to 1000 we would fsync after 1000 ms had passed. In general we recommend you not set this and use replication for durability and allow the operating system's background flush capabilities as it is more efficient."
+  }, {
+    "property": "index.interval.bytes",
+    "default": "4096",
+    "serverDefaultProperties": "log.index.interval.bytes",
+    "description": "This setting controls how frequently Kafka adds an index entry to it's offset index. The default setting ensures that we index a message roughly every 4096 bytes. More indexing allows reads to jump closer to the exact position in the log but makes the index larger. You probably don't need to change this."
+  }, {
+    "property": "max.message.bytes",
+    "default": "1000000",
+    "serverDefaultProperties": "message.max.bytes",
+    "description": "This is largest message size Kafka will allow to be appended to this topic. Note that if you increase this size you must also increase your consumer's fetch size so they can fetch messages this large."
+  }, {
+    "property": "min.cleanable.dirty.ratio",
+    "default": "0.5",
+    "serverDefaultProperties": "log.cleaner.min.cleanable.ratio",
+    "description": "This configuration controls how frequently the log compactor will attempt to clean the log (assuming log compaction is enabled). By default we will avoid cleaning a log where more than 50% of the log has been compacted. This ratio bounds the maximum space wasted in the log by duplicates (at 50% at most 50% of the log could be duplicates). A higher ratio will mean fewer, more efficient cleanings but will mean more wasted space in the log."
+  }, {
+    "property": "min.insync.replicas",
+    "default": "1",
+    "serverDefaultProperties": "min.insync.replicas",
+    "description": "When a producer sets acks to \"all\", min.insync.replicas specifies the minimum number of replicas that must acknowledge a write for the write to be considered successful. If this minimum cannot be met, then the producer will raise an exception (either NotEnoughReplicas or NotEnoughReplicasAfterAppend). When used together, min.insync.replicas and acks allow you to enforce greater durability guarantees. A typical scenario would be to create a topic with a replication factor of 3, set min.insync.replicas to 2, and produce with acks of \"all\". This will ensure that the producer raises an exception if a majority of replicas do not receive a write."
+  }, {
+    "property": "retention.bytes",
+    "default": "None",
+    "serverDefaultProperties": "log.retention.bytes",
+    "description": "This configuration controls the maximum size a log can grow to before we will discard old log segments to free up space if we are using the \"delete\" retention policy. By default there is no size limit only a time limit."
+  }, {
+    "property": "retention.ms",
+    "default": "7 days",
+    "serverDefaultProperties": "log.retention.minutes",
+    "description": "This configuration controls the maximum time we will retain a log before we will discard old log segments to free up space if we are using the \"delete\" retention policy. This represents an SLA on how soon consumers must read their data."
+  }, {
+    "property": "segment.bytes",
+    "default": "1 GB",
+    "serverDefaultProperties": "log.segment.bytes",
+    "description": "This configuration controls the segment file size for the log. Retention and cleaning is always done a file at a time so a larger segment size means fewer files but less granular control over retention."
+  }, {
+    "property": "segment.index.bytes",
+    "default": "10 MB",
+    "serverDefaultProperties": "log.index.size.max.bytes",
+    "description": "This configuration controls the size of the index that maps offsets to file positions. We preallocate this index file and shrink it only after log rolls. You generally should not need to change this setting."
+  }, {
+    "property": "segment.ms",
+    "default": "7 days",
+    "serverDefaultProperties": "log.roll.hours",
+    "description": "This configuration controls the period of time after which Kafka will force the log to roll even if the segment file isn't full to ensure that retention can delete or compact old data."
+  }, {
+    "property": "segment.jitter.ms",
+    "default": "0",
+    "serverDefaultProperties": "log.roll.jitter.{ms,hours}",
+    "description": "The maximum jitter to subtract from logRollTimeMillis."
+  }];
