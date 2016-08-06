4553037d1692b86399a033466a22e3514c43ef9a
Antonios <antwnis@gmail.com>
2016-08-06 22:44:56 +0100

    Minor source-tree fix

--- Gruntfile.js
+++ Gruntfile.js
@@ -23,7 +23,7 @@ module.exports = function (grunt) {
           'src/**/*.html',
           'src/*.html',
           'bower_components/angular/angular.min.js',
-          'bower_components/angular/angular.min.map',                    // Include source-maps
+          'bower_components/angular/angular.min.js.map',                 // Include source-maps
           'bower_components/angular-spinner/angular-spinner.min.js',
           'bower_components/angular-spinner/angular-spinner.min.js.map', // Include source-maps
           'bower_components/bootstrap/dist/css/bootstrap.min.css',
