546e2b25789977d3c46dfee8fb2d30a72c599ffb
Marios Andreopoulos <opensource@andmarios.com>
2016-08-21 23:16:48 +0300

    Fix docker to work with 0.2 release (schema registry and kafka proxy url didn't work), follow better practices for Dockerfile, make image smaller, update readme, disable minify to avoid issues.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- docker/run.sh
+++ docker/run.sh
@@ -2,9 +2,11 @@
 
 if [[ -z "$SCHEMAREGISTRY_UI_URL" ]]; then
     echo "Schema Registry URL was not set via SCHEMAREGISTRY_UI_URL environment variable."
+    sed -e 's|^\s*urlSchema:.*|      urlSchema: ""|' -i /kafka-topics-ui/combined.js
 else
     echo "Setting Schema Registry UI URL to $SCHEMAREGISTRY_UI_URL."
     sed -e 's|var UI_SCHEMA_REGISTRY =.*|var UI_SCHEMA_REGISTRY = "'"$SCHEMAREGISTRY_UI_URL"'";|' \
+        -e 's|^\s*urlSchema:.*|      urlSchema: "'"$SCHEMAREGISTRY_UI_URL"'"|' \
         -i /kafka-topics-ui/combined.js
 fi
 
@@ -13,6 +15,7 @@ if [[ -z "$KAFKA_REST_PROXY_URL" ]]; then
 else
     echo "Kafka REST Proxy URL to $KAFKA_REST_PROXY_URL."
     sed -e 's|var KAFKA_REST =.*|var KAFKA_REST = "'"$KAFKA_REST_PROXY_URL"'";|' \
+        -e 's|KAFKA_REST:.*|KAFKA_REST: "'"$KAFKA_REST_PROXY_URL"'",|' \
         -i /kafka-topics-ui/combined.js
 fi
 
