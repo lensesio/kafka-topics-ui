8fb1720f9a4ccb3e795787d46192122530f1ca55
Christina Daskalaki <chdask@gmail.com>
2016-10-11 19:00:30 +0100

    fix the flat function for primitives

--- src/kafka-topics/view/view.controller.js
+++ src/kafka-topics/view/view.controller.js
@@ -520,90 +520,65 @@ angularAPP.controller('ViewTopicCtrl', function ($scope, $rootScope, $filter, $r
     	return toReturn;
     };
 
-//TODO REFACTOR!!!
-    function flattenTable(rows) {
+ function flattenTable(rows) {
+
         var extraColumnsNumberValue = 0;
         var extraColumnsNumberKey = 0;
         var rowWithMoreColumns;
         $scope.flatRows = [];
-        if (rows.length > 0) { // check if topics exist
+        if (rows.length > 0) {
             angular.forEach(rows, function (row) {
-                  if (row.key == undefined) row.key = ''; //for rendering purposes
-                  if (row.value == undefined) row.value = ''; //for rendering purposes
-
-                  var flatValue ;// = flattenObject(row.value);
-                  var flatKey ;//= flattenObject(row.key);
-                  var rowExtraColumnsValues;
-                  var rowExtraColumnsKeys;
-
-                  rowWithMoreColumns = row;
-
-                  var newRow = {
-                       "offset" : rowWithMoreColumns.offset,
-                       "partition" : rowWithMoreColumns.partition,
-                       "key" : rowWithMoreColumns.key,
-                       "value" : rowWithMoreColumns.value
-                       }
+                  if (row.key == undefined || row.key == null) row.key = '';
+                  if (row.value == undefined || row.value == null) row.value = '';
 
                   if(angular.isNumber(row.value) || angular.isString(row.value)) {
-                      flatValue = row.value;
-                      flatKey = row.key;
-                      rowExtraColumnsValues = 1;
-                      rowExtraColumnsKeys = 1;
+                        extraColumnsNumberValue = 0
+                        extraColumnsNumberKey = 0
+                        $scope.cols = Object.keys(flattenObject(row));
+                        $scope.cols2 = [];
+                        $scope.cols3 = [];
+                  } else {
+                        var flatValue = flattenObject(row.value);
+                        var flatKey = flattenObject(row.key);
+                        var rowExtraColumnsValues = Object.keys(flatValue).length;
+                        var rowExtraColumnsKeys = Object.keys(flatKey).length;
 
-                      $scope.cols = Object.keys(flattenObject(newRow));
-                      $scope.cols2 = [];
-                      $scope.cols3 = [];
-                    } else {
-                      flatValue = flattenObject(row.value);
-                      flatKey = flattenObject(row.key);
-                      rowExtraColumnsValues = Object.keys(flatValue).length;
-                      rowExtraColumnsKeys = Object.keys(flatKey).length;
+                        if(extraColumnsNumberValue < rowExtraColumnsValues) {
+                            extraColumnsNumberValue = rowExtraColumnsValues;
+                            rowWithMoreColumns = row;
+                        }
+
+                        if(extraColumnsNumberKey < rowExtraColumnsKeys) {
+                            extraColumnsNumberKey = rowExtraColumnsKeys;
+                            rowWithMoreColumns = row;
+                        }
+
+                        var newRow = {
+                            "offset" : rowWithMoreColumns.offset,
+                            "partition" : rowWithMoreColumns.partition,
+                            "key" : rowWithMoreColumns.key,
+                            "value" : rowWithMoreColumns.value
+                        }
 
                         $scope.cols =  Object.keys(flattenObject(newRow));
-                        $scope.cols2 = Object.keys(flattenObject(newRow.value)); //only the value cols, TODO same for keys?
-                        $scope.cols3 = Object.keys(flattenObject(newRow.key)); //only the value cols, TODO same for keys?
-                    }
+                        $scope.cols2 = Object.keys(flattenObject(newRow.value));
+                        $scope.cols3 = Object.keys(flattenObject(newRow.key));
+                  }
 
-                    if(extraColumnsNumberValue < rowExtraColumnsValues) {
-                      extraColumnsNumberValue = rowExtraColumnsValues;
-                    }
-
-                    if(extraColumnsNumberKey < rowExtraColumnsKeys) {
-                      extraColumnsNumberKey = rowExtraColumnsKeys;
-                    }
-
-                  //2. create array with flat rows for the flat table
                   $scope.flatRows.push(flattenObject(row));
 
                 });
 
-                //3. reorder the columns for the iteration
                 $scope.extraColsNumValues = extraColumnsNumberValue;
                 $scope.extraColsNumKeys = extraColumnsNumberKey;
 
-        //PAGINATION STUFF
          $scope.paginationItems = 10;
          $scope.showHideAllButtonLabel = 'show ' + rows.length;
-         $scope.showAll = function () {
-            if($scope.paginationItems == 12) {
-               $scope.showHideAllButtonLabel = 'show less';
-               $scope.paginationItems = $scope.flatRows.length;
-            } else {
-               $scope.showHideAllButtonLabel = 'show ' + rows.length;
-               $scope.paginationItems = 12;
-            }
-
-         };
      }
 }
 
  $scope.showTree = function (keyOrValue) {
-    if(angular.isNumber(keyOrValue) || angular.isString(keyOrValue) || (keyOrValue==null)) {
-        return false;
-    } else {
-        return true;
-    }
+    return !(angular.isNumber(keyOrValue) || angular.isString(keyOrValue) || (keyOrValue==null));
  }
 
 });
