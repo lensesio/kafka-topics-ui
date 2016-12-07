8d5d92e77558f2b5da75e198891241793e46826c
John Glampedakis <jglambed@gmail.com>
2016-12-07 19:53:05 +0200

    Fixes #29

--- src/env.js
+++ src/env.js
@@ -11,10 +11,10 @@ var KAFKA_REST_ENV = {
   // Pre-configure the Data Type on particular well-known topics
   JSON_TOPICS: ["_schemas"],
 
-  BINARY_TOPICS: ["connect-configs", "connect-offsets", "connect-status", "__consumer_offsets", "_confluent-monitoring", "_confluent-controlcenter", "__confluent.support.metr"],
+  BINARY_TOPICS: ["connect-configs", "connect-offsets", "__consumer_offsets", "_confluent-monitoring", "_confluent-controlcenter", "__confluent.support.metr"],
 
   // If a topic starts with this particular prefix - it's a control topic
-  CONTROL_TOPICS: ["_confluent-controlcenter", "connect-configs", "connect-offsets", "__confluent", "__consumer_offsets", "_confluent-monitoring"],
+  CONTROL_TOPICS: ["_confluent-controlcenter", "connect-configs", "connect-offsets", "__confluent", "__consumer_offsets", "_confluent-monitoring", "connect-status", "_schemas"],
 
 };
 
