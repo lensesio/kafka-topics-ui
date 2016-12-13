db33d3f7258a9344fec7dbca90aab7dd099de778
Christina <christina@Antonioss-MacBook-Pro.local>
2016-12-13 22:28:46 +0000

    format env

--- env.js
+++ env.js
@@ -1,21 +1,12 @@
 // Replace with the URL where a Kafka REST service is listening
-
-var clusters =
-[
-  {
-  NAME:"prod",
-  KAFKA_REST : "localhost:8082",// "https://kafka-rest-proxy.demo.landoop.com"
-  // Sets the defaul maximum amount of bytes to fetch from each topic
-  MAX_BYTES: "?max_bytes=50000",
-  COLOR: "#141414" // Optional
-
-  },
-  {
-  NAME:"dev",
-  KAFKA_REST : "localhost:8083",// "https://kafka-rest-proxy.demo.landoop.com"
-  // Sets the defaul maximum amount of bytes to fetch from each topic
-  MAX_BYTES: "?max_bytes=50000",
-  COLOR: "red" // Optional
-  }
-];
-
+var clusters = [{
+        NAME: "prod",
+        KAFKA_REST: "localhost:8082",
+        MAX_BYTES: "?max_bytes=50000", 	// Sets the defaul maximum amount of bytes to fetch from each topic
+        COLOR: "#141414" // Optional
+    }, {
+        NAME: "dev",
+        KAFKA_REST: "localhost:8083",
+        MAX_BYTES: "?max_bytes=50000",
+        COLOR: "red"
+    }];
