ddc407b3099e524fc2ea514a0f311489cf7d6db5
antonioschalkiopoulos <antonios.chalkiopoulos@dataffirm.com>
2016-12-17 09:38:09 +0000

    .trim() when getting env variables, to correct typing mistakes i.e. a space at the end

--- src/factories/env.factory.js
+++ src/factories/env.factory.js
@@ -1,30 +1,28 @@
 angularAPP.factory('env', function ($rootScope) {
 
-
   var clusterArray = (typeof clusters !== "undefined") ? angular.copy(clusters) : [];
   var selectedCluster = null;
   setCluster();
 
   return {
-    setSelectedCluster : function(clusterName) { setCluster(clusterName)},
+    setSelectedCluster : function(clusterName) { setCluster(clusterName) },
     getSelectedCluster : function() { return selectedCluster; },
-    getClusters : function() { return clusters} ,
-    KAFKA_REST : function () { return selectedCluster.KAFKA_REST; },
-    MAX_BYTES : function () { return selectedCluster.MAX_BYTES; }
-  }
+    getClusters : function() { return clusters },
+    KAFKA_REST : function () { return selectedCluster.KAFKA_REST.trim(); },
+    MAX_BYTES : function () { return selectedCluster.MAX_BYTES.trim(); }
+  };
 
   function setCluster(clusterName) {
     if(clusterArray.length == 0) {
         $rootScope.missingEnvJS = true;
-              console.log("NOT EXISTS env.js")
+              console.log("File [env.js] does not exist")
      }
      if(angular.isUndefined(clusterName)) {
           selectedCluster = clusterArray[0];
      } else {
-          var filteredArray = clusterArray.filter(function(el) {return el.NAME == clusterName})
+          var filteredArray = clusterArray.filter(function(el) {return el.NAME == clusterName});
           selectedCluster = filteredArray.length == 1 ?  filteredArray[0]  : clusterArray[0]
      }
   }
-
 
 });
