6897df1427bcde8866a0f78a0bdba555872b4fca
Marios Andreopoulos <opensource@andmarios.com>
2016-12-21 21:12:55 +0200

    Update docker image with Kafka Topics UI 0.8.0.
    Signed-off-by: Marios Andreopoulos <opensource@andmarios.com>

--- docker/run.sh
+++ docker/run.sh
@@ -11,23 +11,19 @@ EOF
 KAFKA_REST_PROXY_URL=/api/kafka-rest-proxy
 fi
 
-# if [[ -z "$SCHEMAREGISTRY_UI_URL" ]]; then
-#     echo "Schema Registry URL was not set via SCHEMAREGISTRY_UI_URL environment variable."
-#     sed -e 's|^\s*urlSchema:.*|      urlSchema: ""|' -i /kafka-topics-ui/combined.js
-# else
-#     echo "Setting Schema Registry UI URL to $SCHEMAREGISTRY_UI_URL."
-#     sed -e 's|var UI_SCHEMA_REGISTRY =.*|var UI_SCHEMA_REGISTRY = "'"$SCHEMAREGISTRY_UI_URL"'";|' \
-#         -e 's|^\s*urlSchema:.*|      urlSchema: "'"$SCHEMAREGISTRY_UI_URL"'"|' \
-#         -i /kafka-topics-ui/combined.js
-# fi
-
 if [[ -z "$KAFKA_REST_PROXY_URL" ]]; then
     echo "Kafka REST Proxy URL was not set via KAFKA_REST_PROXY_URL environment variable."
 else
     echo "Kafka REST Proxy URL to $KAFKA_REST_PROXY_URL."
-    sed -e 's|var KAFKA_REST =.*|var KAFKA_REST = "'"$KAFKA_REST_PROXY_URL"'";|' \
-        -e 's|KAFKA_REST:.*|KAFKA_REST: "'"$KAFKA_REST_PROXY_URL"'",|' \
-        -i /kafka-topics-ui/combined.js
+    cat <<EOF >kafka-topics-ui/env.js
+var clusters = [
+   {
+     NAME:"default",
+     KAFKA_REST: "$KAFKA_REST_PROXY_URL",
+     MAX_BYTES: "?max_bytes=50000"
+   }
+]
+EOF
 fi
 
 echo
