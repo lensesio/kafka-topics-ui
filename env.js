145747422ba6046919456fdfd46e48068bd10bbf
John Glampedakis <jglambed@gmail.com>
2017-01-09 13:19:43 +0200

    typos and readme changes

--- env.js
+++ env.js
@@ -2,7 +2,7 @@
 var clusters = [{
         NAME: "prod",
         KAFKA_REST: "localhost:8082",
-        MAX_BYTES: "?max_bytes=50000", 	// Sets the defaul maximum amount of bytes to fetch from each topic
+        MAX_BYTES: "?max_bytes=50000", 	// Sets the default maximum amount of bytes to fetch from each topic
         COLOR: "#141414" // Optional
     }, {
         NAME: "dev",
