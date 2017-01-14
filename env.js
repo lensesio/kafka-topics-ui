6aea7787c4852996b639615ac993942f7153b50b
Christina Daskalaki <chdask@gmail.com>
2017-01-14 20:18:36 +0000

    christina's changes

--- env.js
+++ env.js
@@ -4,7 +4,7 @@ var clusters = [{
         KAFKA_REST: "https://kafka-rest-proxy.demo.landoop.com",
         KAFKA_BACKEND: "http://fast-data-backend.demo.landoop.com/api/rest",
         MAX_BYTES: "?max_bytes=50000", 	// Sets the default maximum amount of bytes to fetch from each topic
-        COLOR: "#141414" // Optional
+        //COLOR: "#141414" // Optional
     }, {
         NAME: "dev",
         KAFKA_REST: "localhost:8083",
