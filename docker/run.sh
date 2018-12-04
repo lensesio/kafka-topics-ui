6ca49eb8ad6d5d0c5a33cdd942731213c297607a
arevell89 <andy.revell@eurostar.com>
2018-12-04 17:18:35 +0000

    test

--- docker/run.sh
+++ docker/run.sh
@@ -80,8 +80,6 @@ EOF
 #nginx
 apk update
 apk add nginx
-apk add apache2-utils
-htpasswd -b -c /etc/nginx/.htpasswd admin ${PASSWORD}
 cat <<EOF >/etc/nginx/nginx.conf
 events {
   worker_connections  1024;
