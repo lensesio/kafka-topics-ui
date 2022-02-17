45e05c4935564ed1a90f6c9f94b7cccb7101166b
primogen <smnacionales@gmail.com>
2022-02-17 07:14:46 -0800

    renamed host param

--- docker/run.sh
+++ docker/run.sh
@@ -8,7 +8,7 @@ INSECURE_PROXY=""
 CADDY_OPTIONS="${CADDY_OPTIONS:-}"
 EXPERIMENTAL_PROXY_URL="${EXPERIMENTAL_PROXY_URL:-false}"
 PORT="${PORT:-8000}"
-
+UI_HOST="${UI_HOST:-localhost}"
 {
     echo "Lenses.io Kafka Topics UI ${KAFKA_TOPICS_UI_VERSION}"
     echo "Visit <https://github.com/lensesio/kafka-topics-ui/tree/master/docker>"
@@ -16,7 +16,7 @@ PORT="${PORT:-8000}"
     echo
 
     cat /caddy/Caddyfile.template \
-        | sed -e "s/8000/$PORT/" > /tmp/Caddyfile
+        | sed -e "s/8000/$PORT/g; s/localhost/$UI_HOST/g" > /tmp/Caddyfile
 
     if echo "$PROXY_SKIP_VERIFY" | egrep -sq "true|TRUE|y|Y|yes|YES|1"; then
         INSECURE_PROXY=insecure_skip_verify
